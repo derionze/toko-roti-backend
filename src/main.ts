d85fe812e70bfe250ffc3322bad25fae70c8c0cf
ReswaraArgyaMunda <argiamc12@gmail.com>
2025-06-08 01:21:48 +0800

    mantap

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
   })
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000, '0.0.0.0');
 }
-// bootstrap();
+bootstrap();
 
 
 // // src/main.ts
